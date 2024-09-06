255d275902054fd3616cf3f8d5c0c8f1aff88859
ceboykin2 <ceboykin2@crimson.ua.edu>
2024-09-06 15:15:18 -0500

    Lab 2

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+//initialize variables
+int numberOfSandwiches = 0;
+int numberOfToppings = 0;
+double tip = 0.0;
+double totalSandwichCost;
+double totalToppingCost;
+double baseCost;
+double orderCost;
+
+//initialize constants
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
+
+//prompt user for # sandwiches
+System.Console.WriteLine("How many sandwiches?");
+numberOfSandwiches = int.Parse(Console.ReadLine());
+
+//prompt user for # toppings
+System.Console.WriteLine("How many toppings?");
+numberOfToppings = int.Parse(Console.ReadLine());
+
+//prompt user for tip
+System.Console.WriteLine("Insert tip amount: ");
+tip = double.Parse(Console.ReadLine());
+
+totalSandwichCost = COST_OF_SANDWICH * numberOfSandwiches;
+totalToppingCost = COST_OF_TOPPING * numberOfToppings;
+baseCost = totalSandwichCost + totalToppingCost;
+
+orderCost = tip + (baseCost * (1-DISCOUNT_AMOUNT));
+
+//display order cost
+System.Console.WriteLine("The total order cost is " + orderCost);
